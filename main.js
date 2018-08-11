217fc3981865679c7d5881afd98f3942265fd1c6
David Basarab <dbasarab617@gmail.com>
2018-08-10 21:51:19 -0400

    using commander

--- main.js
+++ main.js
@@ -1,8 +1,17 @@
 #!/usr/bin/env node
-//https://github.com/chalk/chalk
+ //https://github.com/chalk/chalk
 'use strict';
 
 const playing = require('./lib/playingWithChalk');
+const program = require('commander');
+
+program
+    .version('0.1.0')
+    .option('-p, --peppers', 'Add peppers')
+    .option('-P, --pineapple', 'Add pineapple')
+    .option('-b, --bbq-sauce', 'Add bbq sauce')
+    .option('-c, --cheese [type]', 'Add the specified type of cheese [marble]', 'marble')
+    .parse(process.argv);
 
 function printArgs() {
     // print process.argv
@@ -11,7 +20,14 @@ function printArgs() {
     });
 }
 
-console.log('Will you run this script?');
+console.log('you ordered a pizza with:');
 
-playing.tagYourIt();
+if (program.peppers) console.log('  - peppers');
+if (program.pineapple) console.log('  - pineapple');
+if (program.bbqSauce) console.log('  - bbq');
 
+console.log('  - %s cheese', program.cheese);
+
+//console.log('Will you run this script?');
+
+//playing.tagYourIt();
