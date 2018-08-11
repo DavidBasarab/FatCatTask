248f0651d2ca83b41309b6833abca7c3209e84a6
David Basarab <dbasarab617@gmail.com>
2018-08-10 22:21:36 -0400

    Figured out how to do the parsing

--- main.js
+++ main.js
@@ -1,5 +1,5 @@
 #!/usr/bin/env node
- 
+
 'use strict';
 
 const playing = require('./lib/playingWithChalk');
@@ -9,67 +9,50 @@ const program = require('commander');
 //https://github.com/tj/commander.js/tree/master/examples
 
 // program
-//     .version('0.1.0')
-//     .option('-p, --peppers', 'Add peppers')
-//     .option('-P, --pineapple', 'Add pineapple')
-//     .option('-b, --bbq-sauce', 'Add bbq sauce')
-//     .option('-c, --cheese [type]', 'Add the specified type of cheese [marble]', 'marble')
+//     .version("0.0.1")
+//     .command("install [name]", "do install stuff")
+//     .action(function (name, theName) {
+//         console.log("No actions");
+//         console.log(name);
+//         console.log(theName);
+//         return true;
+//     })
 //     .parse(process.argv);
 
-function printArgs() {
-    // print process.argv
-    process.argv.forEach(function (val, index, array) {
-        console.log(index + ': ' + val);
+program
+    .version('0.0.1')
+    .command('rmdir <dir> [otherDirs...]')
+    .action(function (dir, otherDirs) {
+        console.log('rmdir %s', dir);
+        if (otherDirs) {
+            otherDirs.forEach(function (oDir) {
+                console.log('rmdir %s', oDir);
+            });
+        }
     });
+
+program
+    .command("mkdir <dir> [otherDirs...]")
+    .action(function (dir, otherDirs) {
+        console.log('make!!!!!!!! %s', dir);
+        if (otherDirs) {
+            otherDirs.forEach(function (oDir) {
+                console.log('rmdir %s', oDir);
+            });
+        }
+    });
+
+program
+    .command("install")
+    .action(function () {
+        console.log('Will this do the install');
+    });
+
+program.parse(process.argv);
+
+var NO_COMMAND_SPECIFIED = program.args.length === 0;
+
+// Handle it however you like
+if (NO_COMMAND_SPECIFIED) {
+    console.log("This is a default action");
 }
-
-// console.log('you ordered a pizza with:');
-
-// if (program.peppers) console.log('  - peppers');
-// if (program.pineapple) console.log('  - pineapple');
-// if (program.bbqSauce) console.log('  - bbq');
-
-// console.log('  - %s cheese', program.cheese);
-
-//console.log('Will you run this script?');
-
-//playing.tagYourIt();
-
-// function range(val) {
-//     return val.split('..').map(Number);
-//   }
-   
-//   function list(val) {
-//     return val.split(',');
-//   }
-   
-//   function collect(val, memo) {
-//     memo.push(val);
-//     return memo;
-//   }
-   
-//   function increaseVerbosity(v, total) {
-//     return total + 1;
-//   }
-   
-//   program
-//     .version('0.1.0')
-//     .usage('[options] <file ...>')
-//     .option('-i, --integer <n>', 'An integer argument', parseInt)
-//     .option('-f, --float <n>', 'A float argument', parseFloat)
-//     .option('-r, --range <a>..<b>', 'A range', range)
-//     .option('-l, --list <items>', 'A list', list)
-//     .option('-o, --optional [value]', 'An optional value')
-//     .option('-c, --collect [value]', 'A repeatable value', collect, [])
-//     .option('-v, --verbose', 'A value that can be increased', increaseVerbosity, 0)
-//     .parse(process.argv);
-   
-//   console.log(' int: %j', program.integer);
-//   console.log(' float: %j', program.float);
-//   console.log(' optional: %j', program.optional);
-//   program.range = program.range || [];
-//   console.log(' range: %j..%j', program.range[0], program.range[1]);
-//   console.log(' list: %j', program.list);
-//   console.log(' collect: %j', program.collect);
-//   console.log(' verbosity: %j', program.verbose);
-//   console.log(' args: %j', program.args);
