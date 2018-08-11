b6c3e8d4ea2d531799dbac9560a53e6308e68301
David Basarab <dbasarab617@gmail.com>
2018-08-10 21:39:39 -0400

    adding playing script

--- main.js
+++ main.js
@@ -1,8 +1,17 @@
 #!/usr/bin/env node
 
+'use strict';
+
+const playing = require('./lib/playingWithClass');
+
+function printArgs() {
+    // print process.argv
+    process.argv.forEach(function (val, index, array) {
+        console.log(index + ': ' + val);
+    });
+}
+
 console.log('Will you run this script?');
 
-// print process.argv
-process.argv.forEach(function (val, index, array) {
-    console.log(index + ': ' + val);
-  });
+playing.tagYourIt();
+
